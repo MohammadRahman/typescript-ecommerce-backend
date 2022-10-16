d81d26dc73c175b3f9c2b539506d7f6cb6592512
MohammadRahman <45675266+MohammadRahman@users.noreply.github.com>
2022-10-16 13:55:49 +0300

    feat: new version

--- semantic-release.config.js
+++ semantic-release.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-    branches: 'main',
-    repositoryUrl: 'https://github.com/MohammadRahman/typescript-ecommerce-backend',
-    plugins:[ 
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github'
+  branches: 'main',
+  repositoryUrl: 'https://github.com/MohammadRahman/Phare-CI-CD',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'build.zip', label: 'Build' }
+          // add more path like test coverage
+          // { path: 'coverage.zip', label: 'Coverage' },
+        ]
+      }
     ]
-
-    
-}
+  ]
+};
